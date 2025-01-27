3fafbc0c6290bf66ee9a5e2bd9cb37d4dee1d9aa
JRinor <rinor.januzi7@etu.univ-lorraine.fr>
2025-01-27 17:46:09 +0100

    etape 1 tp 1 finie

--- Pixel.java
+++ Pixel.java
@@ -3,6 +3,7 @@
 // Preparateur: P. Even (Universite de Lorraine / IUT de Saint-Die, Dpt Info)
 // Complete par X
 
+import java.util.ArrayList;
 
 /** Pixel de la grille.
  */
@@ -44,10 +45,56 @@ public class Pixel
    * @param p Le pixel d'arrivee.
    * @return La sequence de pixels du segment.
    */
-  public Pixel[] tracerSegment (Pixel p)
-  {
-    Pixel[] pts = {this, p};
-    return (pts);
+  public Pixel[] tracerSegment(Pixel p) {
+    ArrayList<Pixel> pixels = new ArrayList<>();
+    
+    int x1 = this.x;
+    int y1 = this.y;
+    int x2 = p.x();
+    int y2 = p.y();
+    
+    int dx = Math.abs(x2 - x1);
+    int dy = Math.abs(y2 - y1);
+    
+    int sx = x1 < x2 ? 1 : -1;
+    int sy = y1 < y2 ? 1 : -1;
+
+    
+    boolean raid = dy > dx;
+    if (raid) {
+        int temp = dx;
+        dx = dy;
+        dy = temp;
+    }
+    
+    int r = dx / 2;
+    int x = x1;
+    int y = y1;
+    
+    pixels.add(new Pixel(x, y));
+    
+    for (int i = 0; i < dx; i++) {
+        r += dy;
+        
+        if (r >= dx) {
+            r -= dx;
+            if (raid) {
+                x += sx;
+            } else {
+                y += sy;
+            }
+        }
+        
+        if (raid) {
+            y += sy;
+        } else {
+            x += sx;
+        }
+        
+        pixels.add(new Pixel(x, y));
+    }
+    
+    return pixels.toArray(new Pixel[0]);
   }
 
   /** Fournit les pixels du segment joignant le pixel a un autre.
